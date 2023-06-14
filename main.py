b3f34364ecb4f9b5e9296e236b238424c8358d18
Udokaamos <udokaamos@gmail.com>
2023-06-14 09:09:14 +0200

    converting a string into datetime object

--- main.py
+++ main.py
@@ -4,3 +4,10 @@ import calendar
 import time
 import os
 import pytz
+
+#Converting a string to a datetime object
+datetime_string= '09/19/22 13:55:26'
+# convert datetime string to datetime object
+datetime_object = datetime.strptime(datetime_string, '%m/%d/%y %H:%M:%S')
+
+print(datetime_object)
